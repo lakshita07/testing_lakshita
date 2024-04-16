de50a11f49e71e9bee03ff944f82dca34257cfcf
Lakshita S <137186824+lakshita07@users.noreply.github.com>
2024-04-16 15:50:11 +0530

    Add files via upload

---
+++ day5seleniumph/day5seleniumph/src/test/java/com/example/day5seleniumph/Day5seleniumphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day5seleniumph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day5seleniumphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
